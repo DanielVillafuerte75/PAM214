a047105893d833a0e6a74654d13298cab54fcff0
DanielVillafuerte75 <vdanielisaac@gmail.com>
2025-09-19 21:15:12 -0600

    Practica 2
    Primer ejercicio corregido

---
+++ Practica 2/EjercicioA.js
@@ -0,0 +1,19 @@
+const persona = {
+    nombre: "Ivan Isay",
+    edad: 37,
+direccion: {
+    ciudad: "Qro",
+    pais: "MX"
+}
+};
+
+
+// Aplica desdestructuracion
+
+//Imprime el mensaje
+
+const { nombre, edad, direccion: { ciudad, pais } } = persona;
+
+console.log('Me llamo ' + nombre + ', tengo ' + edad + ' años y vivo en ' + ciudad + ', ' + pais + '.');
+console.log(persona);
+
