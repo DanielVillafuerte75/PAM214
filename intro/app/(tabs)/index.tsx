1ddd784bbcc300336a50dc19a12ca8bfd4a98f20
DanielVillafuerte75 <vdanielisaac@gmail.com>
2025-10-09 17:05:42 -0600

    Practica 5. Componentes Props y State
    en esta practica se realizo una interfaz de un contador con botones  utiles
    trabajando con los componentes

--- intro/app/(tabs)/index.tsx
+++ intro/app/(tabs)/index.tsx
@@ -1,7 +1,14 @@
-import { Text, View } from 'react-native';
+// 1. imports: Zona de importaciones
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
+//2. MAIN: Zona de los componentes 
 export default function TabOneScreen() {
+
+  const [ Contador, setContador]=useState(0);
+
   return (
+    //3. Zona estetica y posicionamiento
     <View style={{ 
       flex: 1, 
       justifyContent: 'center', 
@@ -9,9 +16,11 @@ export default function TabOneScreen() {
       backgroundColor: '#fff'
     }}>
       <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-        Hola mundo De React Native!
+        Contador: {Contador}
       </Text>
+      <Button title="Agregar" onPress={()=>setContador(Contador+1)} />
+      <Button title="Quitar" onPress={()=>setContador(Contador-1)} />
+      <Button title="Reiniciar" onPress={()=>setContador(0)} />
     </View>
   );
 }
-
